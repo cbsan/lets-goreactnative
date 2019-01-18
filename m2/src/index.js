b70fc0287be502fab98b026bdf0c01531479cf8b
cbsan <cbsan.dev@gmail.com>
2019-01-18 13:54:37 -0200

    app m2

---
+++ m2/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import '~/config/reactotron.config';
+import '~/config/dev-tools.config';
+
+import Routes from './routes';
+
+const App = () => <Routes />;
+
+export default App;
